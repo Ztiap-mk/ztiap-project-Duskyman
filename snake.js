6d731c272e979e7f8783d261f17a2e7adbbea447
BuildTools <unconfigured@null.spigotmc.org>
2020-03-28 15:40:35 +0100

    prvy komit

---
+++ snake.js
@@ -0,0 +1,28 @@
+// window.onload = init
+window.addEventListener('load', init)
+
+let game = null;
+
+function init () {
+    // console.log('init()')
+    game = new SnakeGame('platno')
+}
+
+class SnakeGame {
+    constructor(id) {
+        this.ctx = document.getElementById(id).getContext('2d')
+        this.isRunning = false
+        this.lastTime = 0;
+        this.framePeriod = 1 / 60
+    }
+
+    start() {
+        this.isRunning = true
+        this.lastTime = 0
+        requestAnimationFrame((time) => this.tick(time))
+    }
+
+    tick(time) {
+        requestAnimationFrame((time) => this.tick(time))
+    }
+}
