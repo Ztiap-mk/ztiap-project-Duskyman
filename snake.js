8701c79830d716d84c2792e0325335d8ecf6bf30
Duskyman <duskyman7@gmail.com>
2020-03-28 19:32:25 +0100

    tmp

--- snake.js
+++ snake.js
@@ -6,23 +6,79 @@ let game = null
 function init() {
     // console.log('init()')
     game = new SnakeGame('platno')
+    game.start()
+}
+
+const STATES = {
+    NOTHING: {
+        init: () => {},
+        update: () => {},
+        input: (events) => {},
+        dispose: () => {},
+    },
+    GAME_STARTING: {
+        init: () => {},
+        update: () => {},
+        input: (events) => {},
+        dispose: () => {},
+    },
+    GAME_PLAY: {
+        init: () => {},
+        update: () => {},
+        input: (events) => {},
+        dispose: () => {},
+    },
+    GAME_OVER: {
+        init: () => {},
+        update: () => {},
+        input: (events) => {},
+        dispose: () => {},
+    },
 }
 
 class SnakeGame {
     constructor(id) {
-        this.ctx = document.getElementById(id).getContext('2d')
+        this.canvas = document.getElementById(id)
+        this.ctx = this.canvas.getContext('2d')
         this.isRunning = false
-        this.lastTime = 0
-        this.framePeriod = 1 / 60
+        this.lastTick = 0
+        this.framePeriod = 1000 / 60
+        this.setup()
+        this.state = STATES.NOTHING
+        this.events = []
     }
 
     start() {
         this.isRunning = true
-        this.lastTime = 0
+        this.lastTick = 0
         requestAnimationFrame((time) => this.tick(time))
     }
 
+    stop() {
+        this.isRunning = false
+    }
+
+    setup() {
+        window.addEventListener('keydown', (e) => this.input(e))
+        window.addEventListener('keyup', (e) => this.input(e))
+    }
+
     tick(time) {
+        const dt = time - this.lastTick
+        if (dt >= this.framePeriod) {
+            this.lastTick = time
+            this.update(dt)
+            this.render(dt)
+        }
         requestAnimationFrame((time) => this.tick(time))
+    }
+    input(e) {
+        this.events.push({ type: e.type, keyCode: e.keyCode })
+    }
+    update(dt) {}
+    render(dt) {
+        const { ctx, canvas } = this
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        ctx.fillText(`${dt.toFixed(1)}`, 100, 100)
     }
 }
