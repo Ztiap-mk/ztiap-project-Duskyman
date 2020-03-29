4c35d7930fcc6d892c543f0c63cff9606ddffe05
Dušan Javorník <dusan.javornik@vacuumlabs.com>
2020-03-30 00:23:05 +0200

    working countdown

--- entities/textEntity.js
+++ entities/textEntity.js
@@ -1,25 +1,27 @@
-class Text extends Entity {
+class TextEntity extends Entity {
     constructor(options) {
-        this.text = ''
         super(options)
+        this.text = options.text || ''
+        this.textColor = (options && options.textColor) || 'black'
         this.backgroundColor = (options && options.backgroundColor) || 'transparent'
         this.textAlign = 'center'
-        this.textBaseline = 'center'
+        this.textBaseline = 'middle'
         this.fontFamily = 'serif'
         this.fontSize = '20px'
     }
     update() {}
     draw(ctx) {
         ctx.save()
-        ctx.fillStyle = this.backgroundColor
         ctx.textAlign = this.textAlign
         ctx.textBaseline = this.textBaseline
         ctx.font = `${this.fontSize} ${this.fontFamily}`
         ctx.translate(this.position.x, this.position.y)
         ctx.rotate(this.rotation)
         ctx.scale(this.scale.x, this.scale.y)
+        ctx.fillStyle = this.backgroundColor
         ctx.fillRect(-(this.size.x / 2), -(this.size.y / 2), this.size.x, this.size.y)
-        ctx.fillText(this.text, this.position.x, this.position.y)
+        ctx.fillStyle = this.textColor
+        ctx.fillText(this.text, 0, 0)
         ctx.restore()
     }
 }
--- states/gameStartingState.js
+++ states/gameStartingState.js
@@ -1,5 +1,14 @@
 class GameStartingState extends State {
-    init({ buttons, gameData }) {
+    constructor(options) {
+        super(options)
+
+        this.countdown = new TextEntity({
+            text: '3',
+            fontSize: 30
+        })
+    }
+
+    init({ buttons, gameData, canvas }) {
         buttons.newGame.disabled = true
         buttons.pause.disabled = false
         buttons.reset.disabled = false
@@ -10,8 +19,23 @@ class GameStartingState extends State {
                 { direction: 'right', position: { x: 10, y: 5 } },
             ],
         }
+        this.countdownValue = 3
+        this.animation = 0
+        this.countdown.text = String(this.countdownValue)
+        this.countdown.position = {x: canvas.width / 2, y: canvas.height / 2}
     }
+
     update(dt, globals) {
+        this.animation += dt / 1000
+        if (this.animation >= 1) {
+            this.animation -= 1
+            this.countdownValue -= 1
+            if (this.countdownValue)
+            this.countdown.text = String(this.countdownValue)
+        }
+
+        this.countdown.scale = {x: 1 + (1 - this.animation)*5, y: 1 + (1 - this.animation)*5}
+
         while (globals.events.length) {
             const [event] = globals.events.splice(0, 1)
             if (event.type === 'click' && event.id === 'resetBut') {
@@ -22,5 +46,6 @@ class GameStartingState extends State {
 
     render(dt, { ctx }) {
         drawBackground(ctx)
+        this.countdown.draw(ctx)
     }
 }
