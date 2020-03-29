cf074eb02b540420e52cee31da95551847fd8923
Dušan Javorník <dusan.javornik@vacuumlabs.com>
2020-03-29 15:00:46 +0200

    tmp

--- snake.js
+++ snake.js
@@ -9,28 +9,87 @@ function init() {
     game.start()
 }
 
+class Entity {
+    constructor(options) {
+        this.position = {
+            x: 0,
+            y: 0,
+        }
+        this.scale = {
+            x: 1,
+            y: 1,
+        }
+        this.size = {
+            x: 0,
+            y: 0,
+        }
+        this.rotation = 0
+        this.image = null
+        this.backgroundColor = "#000"
+    }
+    draw(ctx) {
+        ctx.save()
+        ctx.translate(this.position.x, this.position.y)
+        ctx.rotate(this.rotation)
+        ctx.scale(this.scale.x, this.scale.y)
+        ctx.fillRect(-(this.size.x / 2), -(this.size.y/2), this.size.x, this.size.y)
+        ctx.restore()
+    }
+}
+
+class NothingState {
+    constructor() {
+        this.text = 'Super Snake 4001'
+        this.entity = new Entity()
+        this.entity.size = {x: 50, y: 50}
+        this.entity.position = {x: 50, y: 50}
+        this.animation = 0
+    }
+    init() {}
+    update (dt) {
+        this.animation = (dt / 3000 + this.animation) % 1
+        this.entity.rotation = this.animation * Math.PI * 2
+    }
+    render (dt, globals) {
+        // globals.ctx.save()
+        globals.ctx.save()
+        globals.ctx.textAlign = 'center'
+        globals.ctx.textBaseline = 'middle'
+        globals.ctx.font = '25px serif'
+        globals.ctx.translate(globals.canvas.width / 2, globals.canvas.height / 2)
+        globals.ctx.rotate((this.animation) * 2*Math.PI * (-1))
+        globals.ctx.scale(1 + Math.sin((this.animation)*Math.PI*2) / 4  , 1 + Math.sin((this.animation)*Math.PI*2) / 4)
+        globals.ctx.fillText(this.text, 0, 0)
+        globals.ctx.restore()
+        this.entity.draw(globals.ctx)
+    }
+    input (events) {}
+    dispose() {}
+}
+
 const STATES = {
-    NOTHING: {
-        init: () => {},
-        update: () => {},
-        input: (events) => {},
-        dispose: () => {},
-    },
+    NOTHING: new NothingState(),
     GAME_STARTING: {
+        data: {},
         init: () => {},
         update: () => {},
+        render: () => {},
         input: (events) => {},
         dispose: () => {},
     },
     GAME_PLAY: {
+        data: {},
         init: () => {},
         update: () => {},
+        render: () => {},
         input: (events) => {},
         dispose: () => {},
     },
     GAME_OVER: {
+        data: {},
         init: () => {},
         update: () => {},
+        render: () => {},
         input: (events) => {},
         dispose: () => {},
     },
@@ -46,6 +105,13 @@ class SnakeGame {
         this.setup()
         this.state = STATES.NOTHING
         this.events = []
+        this.nextState = null
+        this.globals = {
+            canvas: this.canvas,
+            ctx: this.ctx,
+            events: this.events,
+            changeState: (state) => this.changeState(state)
+        }
     }
 
     start() {
@@ -66,6 +132,11 @@ class SnakeGame {
     tick(time) {
         const dt = time - this.lastTick
         if (dt >= this.framePeriod) {
+            if (this.nextState) {
+                this.state.dispose()
+                this.state = this.nextState
+                this.nextState = null
+            }
             this.lastTick = time
             this.update(dt)
             this.render(dt)
@@ -75,10 +146,18 @@ class SnakeGame {
     input(e) {
         this.events.push({ type: e.type, keyCode: e.keyCode })
     }
-    update(dt) {}
+    update(dt) {
+        this.state.update(dt)
+    }
     render(dt) {
         const { ctx, canvas } = this
         ctx.clearRect(0, 0, canvas.width, canvas.height)
         ctx.fillText(`${dt.toFixed(1)}`, 100, 100)
+        this.state.render(dt, this.globals)
+    }
+
+    changeState(state) {
+        this.nextState = state
     }
 }
+
